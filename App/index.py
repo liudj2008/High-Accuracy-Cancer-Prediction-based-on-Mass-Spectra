19d7ba96d931b609bf3a8539f64bf60684dc6fc1
liudj2008 <liudj2008@gmail.com>
2018-06-30 20:00:52 -0500

    new app

--- App/index.py
+++ App/index.py
@@ -433,7 +433,6 @@ def select_molecules(group, low_limit, high_limit):
 
 ################################################################################
 app = dash.Dash(__name__)
-server = app.server
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
